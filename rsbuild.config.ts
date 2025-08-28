a3383df9dabc19b9d6a8a2cd75be1c7366f973f8
Reuben <1557519+alt-ctrl-dev@users.noreply.github.com>
2025-08-28 22:04:16 +0800

    initial rsbuild setup

---
+++ rsbuild.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@rsbuild/core';
+
+export default defineConfig({
+    html: {
+        template: './static/index.html',
+        title: process.env.APP_TITLE??"CSS Animation using sprite",
+    },
+});
