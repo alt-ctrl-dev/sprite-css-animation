ebb6d81e0e9dc79211109c54503948bf976e28f3
Reuben <1557519+alt-ctrl-dev@users.noreply.github.com>
2025-08-28 23:42:03 +0800

    add config for github page

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from '@rsbuild/core';
 export default defineConfig({
     html: {
         template: './static/index.html',
-        title: process.env.APP_TITLE??"CSS Animation using sprite",
+        title: process.env.APP_TITLE ?? "CSS Animation using sprite",
+    },
+    output: {
+        assetPrefix: '/sprite-css-animation/',
     },
 });
